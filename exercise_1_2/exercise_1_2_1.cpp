a4148936a0684f6e8004ddc15b6b9507e71aeda1
Archishmaan Peyyety <archishou@gmail.com>
2020-10-12 08:34:49 +0000

    ex 3

--- exercise_1_2/exercise_1_2_1.cpp
+++ exercise_1_2/exercise_1_2_1.cpp
@@ -1,34 +1,38 @@
 #include <iostream>
 #include <vector>
 
-int main()
-{
+int main() {
     // Part 1: Vector Reversal
 
     // Input vector from command-line
     std::vector<int> list;
     int input = 0;
     std::cout << "Input List Of Numbers (end list with non-integer):" << std::endl;
-    while (std::cin >> input)
-    {
+    while (std::cin >> input) {
         list.push_back(input);
     }
 
     // Print original vector
     std::cout << "Original Vector: ";
-    for (int v : list)
-    {
+    for (int v : list) {
         std::cout << v << " ";
     }
     std::cout << std::endl;
 
     // Reverse Vector
     // WRITE YOUR VECTOR REVERSAL HERE
-
+    int start = 0;
+    int end = list.size() - 1;
+    while (start < end) {
+	    int start_init = list[start];
+	    list[start] = list[end];
+	    list[end] = start_init;
+	    start++;
+	    end--;
+    }
     // Print the Reversed vector
     std::cout << "Reversed Vector: ";
-    for (int v : list)
-    {
+    for (int v : list) {
         std::cout << v << " ";
     }
     std::cout << std::endl;
